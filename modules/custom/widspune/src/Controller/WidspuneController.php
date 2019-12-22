4239b2f879ff1deb7492bdc3b2325fb9049acac7
rahul.shinde <shinde.r.a@gmail.com>
2019-12-22 16:59:28 +0530

    Blocked users without frame image.

--- modules/custom/widspune/src/Controller/WidspuneController.php
+++ modules/custom/widspune/src/Controller/WidspuneController.php
@@ -14,6 +14,23 @@ class WidspuneController extends ControllerBase {
    */
   public function build() {
 
+//    $entity_type_manager = \Drupal::service('entity_type.manager');
+//    $user_storage = $entity_type_manager->getStorage('user');
+//
+//    $query = $user_storage->getQuery();
+//    $query->condition('status', 1);
+//    $query->condition('field_photo_with_frame', NULL, 'IS NULL');
+//    $query->condition('roles', 'volunteer');
+//    $query->sort('uid');
+//    $pl_item_ids = $query->execute();
+//
+//    $users = $user_storage->loadMultiple($pl_item_ids);
+//
+//    foreach ($users as $user) {
+//      $user->set('status', 0);
+//      $user->save();
+//    }
+
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t('It works!'),
